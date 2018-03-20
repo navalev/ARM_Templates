a69ed2b86bc26cefff4a68205801be6a523f15bd
navalev <navale@microsoft.com>
2018-03-20 20:13:20 +0200

    setup script

--- ZertoAzure2Azure/deploy.ps1
+++ ZertoAzure2Azure/deploy.ps1
@@ -1,12 +1,12 @@
-$parameters = @{
-    "vmAdminUserName" = "azure",
-    "vmAdminPassword" = "Admin!234567"
-}
-
+$user = Read-Host -Prompt 'VM user name'
+$password = Read-Host -AsSecureString -Prompt 'password'
 $sourceRG = "source"
 $targerRG = "target"
 
 New-AzureRmResourceGroup -Name $sourceRG -Location 'West Europe'
-New-AzureRmResourceGroupDeployment -ResourceGroupName $sourceRG -TemplateFile zertodeploysource.json -TemplateParameterObject $parameters
+$sourceVnet = New-AzureRmResourceGroupDeployment -ResourceGroupName $sourceRG -TemplateFile .\zertodeploysource.json -vmAdminUserName $user -vmAdminPassword $password
+
 New-AzureRmResourceGroup -Name $targerRG -Location 'West Europe'
-New-AzureRmResourceGroupDeployment -ResourceGroupName $targerRG -TemplateFile zertodeploytarget.json -TemplateParameterObject $parameters
+$targetVnet = New-AzureRmResourceGroupDeployment -ResourceGroupName $targerRG -TemplateFile .\zertodeploytarget.json -vmAdminUserName $user -vmAdminPassword $password -remoteVnetResource $sourceVnet.Outputs.vNet
+
+New-AzureRmResourceGroupDeployment -ResourceGroupName $sourceRG -TemplateFile .\zertodeploypeering.json -existingSourceVirtualNetworkName $sourceVnet.Outputs.vNet -existingTargetVirtualNetworkId $targetVnet.Outputs.vNet

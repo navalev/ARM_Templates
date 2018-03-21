0f02d7106350c19f400005fd5fd69fcb768d2e52
navalev <navale@microsoft.com>
2018-03-21 12:20:55 +0200

    setup azure2azure with 2 RG

--- ZertoAzure2Azure/README.md
+++ ZertoAzure2Azure/README.md
@@ -1,3 +1,16 @@
-<a href="https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Fnavalev%2FARM_Templates%2Fmaster%2FZertoAzure2Azure%2Fazuredeploy.json" target="_blank">
-    <img src="http://azuredeploy.net/deploybutton.png"/>
-</a>
+# Zerto Virtual Replication - Azure to Azure
+
+This sample will setup 2 Azure resource groups - **source** and **target**.
+Each group will have a ZCA instance installed. The **source** resouce group will also have a Windows Server Virtual Machine to replicate into **target** resource group once ZCA is configured.
+
+## Setup with Powershell
+1. Clone or download this repo to the installation machine
+2. Make sure you have [Azure Powershell](https://docs.microsoft.com/en-us/powershell/azure/install-azurerm-ps?view=azurermps-5.5.0) installed
+3. Login to your Azure account:
+```powershell
+Login-AzureRmAccount
+```
+4. Execute [deploy.ps1](./deploy.ps1). You will be prompted for username and password for the Virtual Machines to be created. Note that all VMs will have the same username and password in this sample.
+```powershell
+.\deploy.ps1
+```
--- ZertoAzure2Azure/deploy.ps1
+++ ZertoAzure2Azure/deploy.ps1
@@ -3,10 +3,13 @@ $password = Read-Host -AsSecureString -Prompt 'password'
 $sourceRG = "source"
 $targerRG = "target"
 
+# Create Source 
 New-AzureRmResourceGroup -Name $sourceRG -Location 'West Europe'
 $sourceVnet = New-AzureRmResourceGroupDeployment -ResourceGroupName $sourceRG -TemplateFile .\zertodeploysource.json -vmAdminUserName $user -vmAdminPassword $password
 
+# Create Target and peering (target --> source)
 New-AzureRmResourceGroup -Name $targerRG -Location 'West Europe'
-$targetVnet = New-AzureRmResourceGroupDeployment -ResourceGroupName $targerRG -TemplateFile .\zertodeploytarget.json -vmAdminUserName $user -vmAdminPassword $password -remoteVnetResource $sourceVnet.Outputs.vNet
+$targetVnet = New-AzureRmResourceGroupDeployment -ResourceGroupName $targerRG -TemplateFile .\zertodeploytarget.json -vmAdminUserName $user -vmAdminPassword $password -remoteVnetResource $sourceVnet.Outputs.vNet.Value
 
-New-AzureRmResourceGroupDeployment -ResourceGroupName $sourceRG -TemplateFile .\zertodeploypeering.json -existingSourceVirtualNetworkName $sourceVnet.Outputs.vNet -existingTargetVirtualNetworkId $targetVnet.Outputs.vNet
+# Setup peering (source --> target)
+New-AzureRmResourceGroupDeployment -ResourceGroupName $sourceRG -TemplateFile .\zertodeploypeering.json -existingSourceVirtualNetworkName $sourceVnet.Outputs.vNetName.Value -existingTargetVirtualNetworkId $targetVnet.Outputs.vNet.Value
